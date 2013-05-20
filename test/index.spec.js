b2b52828098f744da238285453c318401bd37536
FL4TLiN3 <lambda.groove@gmail.com>
2013-05-20 22:12:47 +0900

    support random string

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+
+var path = require('path'),
+    chai = require('chai');
+
+var utsusemi = require('..');
+
+describe('Utsusemi', function() {
+    describe('#init', function() {
+
+        it('init', function(done){
+            done();
+        });
+
+    });
+});
