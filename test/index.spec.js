80db77a34442a0fe173660409daa142a3e7a0586
FL4TLiN3 <lambda.groove@gmail.com>
2013-05-21 12:22:05 +0900

    start using supertest, and support --port option

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,30 @@
 
 var path = require('path'),
+    request = require('supertest'),
     chai = require('chai');
 
-var utsusemi = require('..');
+var Utsusemi = require('..'),
+    utsusemi = new Utsusemi({
+        port: 3002
+    });
 
 describe('Utsusemi', function() {
+    before(function(done) {
+        utsusemi.add(path.join(__dirname, '../support/'));
+        utsusemi.run();
+        done();
+    });
+
     describe('#init', function() {
 
         it('init', function(done){
             done();
         });
 
+    });
+
+    after(function(done) {
+        utsusemi.shutdown();
+        done();
     });
 });
